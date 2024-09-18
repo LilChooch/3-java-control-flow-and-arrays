c3af4cfc2e1a242e3c6d89f971a581b29e13c119
LilChooch <oleg.2018.85.ru@gmail.com>
2024-09-19 02:59:28 +0500

    Задание 13 Смолин Олег При-202

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -11,9 +11,28 @@ public class Task13Main {
          */
     }
 
-    static int[] removeMoreThen1000(int[] arr) {
+    static int[] removeMoreThen1000(int[] arr)
+    {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        int[] newarr = new int[]{};
+        if(arr == null || arr.length == 0 )
+        {
+
+        }
+        else
+        {
+
+            newarr = arr;
+            for (int i = 0; i < arr.length; i++)
+            {
+                if (arr[i] <= 999)
+                {
+                    newarr[i] = arr[i];
+                }
+            }
+
+        }
+        return newarr;
     }
 
 }
